cbb52f2cb2e9069eddeeb5a5702e72212b0043c0
Ojutiku Oluwatobi <ojutiku.oluwatobi@gmail.com>
2020-04-04 08:31:19 +0400

    Updated react html multi-select

--- dist/index.js
+++ dist/index.js
@@ -1,186 +1,236 @@
 "use strict";
 
-var _interopRequire = function (obj) { return obj && obj.__esModule ? obj["default"] : obj; };
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
 
-var _createClass = (function () { function defineProperties(target, props) { for (var key in props) { var prop = props[key]; prop.configurable = true; if (prop.value) prop.writable = true; } Object.defineProperties(target, props); } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _react = _interopRequireDefault(require("react"));
 
-var _get = function get(object, property, receiver) { var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc && desc.writable) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
+var _classnames = _interopRequireDefault(require("classnames"));
 
-var _inherits = function (subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) subClass.__proto__ = superClass; };
+var _propTypes = _interopRequireDefault(require("prop-types"));
 
-var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-var React = _interopRequire(require("react"));
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
-var ReactDOM = _interopRequire(require("react-dom"));
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var classNames = _interopRequire(require("classnames"));
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-var Dropdown = (function (_React$Component) {
-  function Dropdown(props) {
-    _classCallCheck(this, Dropdown);
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-    _get(Object.getPrototypeOf(Dropdown.prototype), "constructor", this).call(this, props);
-    this.state = {
-      selected: props.value || [],
-      isOpen: false
-    };
-    this.mounted = true;
-    this.handleDocumentClick = this.handleDocumentClick.bind(this);
-  }
+function _createSuper(Derived) { return function () { var Super = _getPrototypeOf(Derived), result; if (_isNativeReflectConstruct()) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
 
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+var Dropdown = /*#__PURE__*/function (_React$Component) {
   _inherits(Dropdown, _React$Component);
 
-  _createClass(Dropdown, {
-    componentWillReceiveProps: {
-      value: function componentWillReceiveProps(newProps) {
-        if (newProps.value && newProps.value !== this.state.selected) {
-          this.setState({ selected: newProps.value });
-        }
-      }
-    },
-    componentDidMount: {
-      value: function componentDidMount() {
-        document.addEventListener("click", this.handleDocumentClick, false);
-      }
-    },
-    componentWillUpdate: {
-      value: function componentWillUpdate(_newProps, newState) {
-        if (newState.selected !== this.state.selected && this.props.onChange) {
-          this.props.onChange(newState.selected);
-        }
-      }
-    },
-    componentWillUnmount: {
-      value: function componentWillUnmount() {
-        this.mounted = false;
-        document.removeEventListener("click", this.handleDocumentClick, false);
-      }
-    },
-    handleMouseDown: {
-      value: function handleMouseDown(event) {
+  var _super = _createSuper(Dropdown);
 
-        if (event.type == "mousedown" && event.button !== 0) {
-          return;
-        }event.stopPropagation();
-        event.preventDefault();
+  function Dropdown(props) {
+    var _this;
 
-        this.setState({
-          isOpen: !this.state.isOpen
-        });
-      }
-    },
-    setValue: {
-      value: function setValue(option) {
-        var selected = this.state.selected;
+    _classCallCheck(this, Dropdown);
 
-        var optionIndex = selected.indexOf(option);
-        var newState = {
-          selected: optionIndex === -1 ? selected.concat(option) : selected.slice(0, optionIndex).concat(selected.slice(optionIndex + 1))
-        };
-        this.setState(newState);
-      }
-    },
-    renderOption: {
-      value: function renderOption(option) {
-        var optionClass = classNames({
-          "Dropdown-option": true,
-          "is-selected": this.state.selected.indexOf(option) !== -1
-        });
+    _this = _super.call(this, props);
+    _this.topNode = _react["default"].createRef();
+    var value = [];
 
-        return React.createElement(
-          "div",
-          { key: option.value, className: optionClass, onClick: this.setValue.bind(this, option) },
-          option.label
-        );
-      }
-    },
-    buildMenu: {
-      value: function buildMenu() {
-        var _this = this;
-
-        var ops = this.props.options.map(function (option) {
-          if (option.type == "group") {
-            var groupTitle = React.createElement(
-              "div",
-              { className: "title" },
-              option.name
-            );
-            var _options = option.items.map(function (item) {
-              return _this.renderOption(item);
-            });
-
-            return React.createElement(
-              "div",
-              { className: "group", key: option.name },
-              groupTitle,
-              _options
-            );
-          } else {
-            return _this.renderOption(option);
+    if (props.value && props.options) {
+      props.options.forEach(function (opt) {
+        props.value.forEach(function (opt2) {
+          if (opt2.value === opt.value) {
+            value.push(opt);
           }
         });
+      });
+    }
 
-        return ops.length ? ops : React.createElement(
-          "div",
-          { className: "Dropdown-noresults" },
-          "No options found"
-        );
-      }
-    },
-    handleDocumentClick: {
-      value: function handleDocumentClick(event) {
-        if (this.mounted) {
-          if (!ReactDOM.findDOMNode(this).contains(event.target)) {
-            this.setState({ isOpen: false });
-          }
-        }
-      }
-    },
-    render: {
-      value: function render() {
-        var _props = this.props;
-        var className = _props.className;
-        var controlClassName = _props.controlClassName;
-        var menuClassName = _props.menuClassName;
-        var placeholder = _props.placeholder;
-        var noPreview = _props.noPreview;
-        var _state = this.state;
-        var selected = _state.selected;
-        var isOpen = _state.isOpen;
+    _this.state = {
+      selected: value,
+      isOpen: false
+    };
+    _this.handleDocumentClick = _this.handleDocumentClick.bind(_assertThisInitialized(_this));
+    return _this;
+  }
 
-        var value = selected.map(function (option) {
-          return option.label;
-        }).join(", ");
-        var menu = isOpen ? React.createElement(
-          "div",
-          { className: menuClassName },
-          this.buildMenu()
-        ) : null;
-
-        var dropdownClass = classNames({
-          Dropdown: true,
-          "is-open": isOpen
-        }, className);
-
-        return React.createElement(
-          "div",
-          { className: dropdownClass },
-          React.createElement(
-            "div",
-            { className: controlClassName, onMouseDown: this.handleMouseDown.bind(this), onTouchEnd: this.handleMouseDown.bind(this) },
-            !noPreview && value || placeholder || "Select...",
-            React.createElement("span", { className: "Dropdown-arrow" })
-          ),
-          menu
-        );
+  _createClass(Dropdown, [{
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      document.addEventListener("click", this.handleDocumentClick, false);
+    }
+  }, {
+    key: "componentWillUpdate",
+    value: function componentWillUpdate(_newProps, newState) {
+      if (newState.selected !== this.state.selected && this.props.onChange) {
+        this.props.onChange(newState.selected);
       }
     }
-  });
+  }, {
+    key: "componentWillUnmount",
+    value: function componentWillUnmount() {
+      document.removeEventListener("click", this.handleDocumentClick, false);
+    }
+  }, {
+    key: "handleMouseDown",
+    value: function handleMouseDown(event) {
+      if (event.type === "mousedown" && event.button !== 0) return;
+      event.stopPropagation();
+      event.preventDefault();
+      this.setState(function (pState) {
+        return {
+          isOpen: !pState.isOpen
+        };
+      });
+    }
+  }, {
+    key: "setValue",
+    value: function setValue(option) {
+      var selected = this.state.selected;
+      var optionIndex = selected.indexOf(option);
+      var newState = {
+        selected: optionIndex === -1 ? selected.concat(option) : selected.slice(0, optionIndex).concat(selected.slice(optionIndex + 1))
+      };
+      this.setState(newState);
+    }
+  }, {
+    key: "renderOption",
+    value: function renderOption(option) {
+      var optionClass = (0, _classnames["default"])({
+        "Dropdown-option": true,
+        "is-selected": this.state.selected.indexOf(option) !== -1
+      });
+      return /*#__PURE__*/_react["default"].createElement("div", {
+        role: "option",
+        key: option.value,
+        className: optionClass,
+        onClick: this.setValue.bind(this, option),
+        onKeyDown: this.setValue.bind(this, option),
+        tabIndex: "0"
+      }, option.label);
+    }
+  }, {
+    key: "buildMenu",
+    value: function buildMenu() {
+      var _this2 = this;
+
+      var ops = this.props.options.map(function (option) {
+        if (option.type === "group") {
+          var groupTitle = /*#__PURE__*/_react["default"].createElement("div", {
+            className: "title"
+          }, option.name);
+
+          var options = option.items.map(function (item) {
+            return _this2.renderOption(item);
+          });
+          return /*#__PURE__*/_react["default"].createElement("div", {
+            className: "group",
+            key: option.name
+          }, groupTitle, options);
+        }
+
+        return _this2.renderOption(option);
+      });
+      return ops.length ? ops : /*#__PURE__*/_react["default"].createElement("div", {
+        className: "Dropdown-noresults"
+      }, "No options found");
+    }
+  }, {
+    key: "renderHtmlOptions",
+    value: function renderHtmlOptions() {
+      var selectedValues = this.state.selected.map(function (item) {
+        return String(item.value);
+      });
+      return this.props.options.map(function (option) {
+        return /*#__PURE__*/_react["default"].createElement("option", {
+          key: option.value,
+          value: option.value,
+          selected: selectedValues.indexOf(String(option.value)) > -1
+        }, option.label);
+      });
+    }
+  }, {
+    key: "handleDocumentClick",
+    value: function handleDocumentClick(event) {
+      if (!this.topNode.current.contains(event.target)) {
+        this.setState({
+          isOpen: false
+        });
+      }
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var _this$props = this.props,
+          className = _this$props.className,
+          controlClassName = _this$props.controlClassName,
+          menuClassName = _this$props.menuClassName,
+          placeholder = _this$props.placeholder,
+          noPreview = _this$props.noPreview;
+      var _this$state = this.state,
+          selected = _this$state.selected,
+          isOpen = _this$state.isOpen;
+      var value = selected.map(function (option) {
+        return option.label;
+      }).join(", ");
+      var menu = isOpen ? /*#__PURE__*/_react["default"].createElement("div", {
+        className: menuClassName
+      }, this.buildMenu()) : null;
+      var dropdownClass = (0, _classnames["default"])({
+        Dropdown: true,
+        "is-open": isOpen
+      }, className);
+      return /*#__PURE__*/_react["default"].createElement("div", {
+        ref: this.topNode,
+        className: dropdownClass
+      }, /*#__PURE__*/_react["default"].createElement("select", {
+        multiple: true,
+        name: this.props.name,
+        className: "invisible-element"
+      }, this.renderHtmlOptions()), /*#__PURE__*/_react["default"].createElement("div", {
+        role: "listbox",
+        tabIndex: "0",
+        className: controlClassName,
+        onMouseDown: this.handleMouseDown.bind(this),
+        onTouchEnd: this.handleMouseDown.bind(this)
+      }, !noPreview && value || noPreview && selected.length && "".concat(selected.length, " Selected") || placeholder, /*#__PURE__*/_react["default"].createElement("span", {
+        className: "Dropdown-arrow"
+      })), menu);
+    }
+  }]);
 
   return Dropdown;
-})(React.Component);
+}(_react["default"].Component);
 
-Dropdown.defaultProps = { controlClassName: "Dropdown-control", menuClassName: "Dropdown-menu" };
-module.exports = Dropdown;
-
+Dropdown.propTypes = {
+  name: _propTypes["default"].string,
+  value: _propTypes["default"].array,
+  options: _propTypes["default"].array,
+  placeholder: _propTypes["default"].string,
+  onChange: _propTypes["default"].func,
+  controlClassName: _propTypes["default"].string,
+  menuClassName: _propTypes["default"].string,
+  className: _propTypes["default"].string,
+  noPreview: _propTypes["default"].bool
+};
+Dropdown.defaultProps = {
+  controlClassName: "Dropdown-control",
+  menuClassName: "Dropdown-menu",
+  onChange: function onChange() {},
+  placeholder: "Select"
+};
+var _default = Dropdown;
+exports["default"] = _default;
--- index.js
+++ index.js
@@ -1,125 +1,175 @@
-'use strict';
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import classNames from 'classnames';
+import React from "react";
+import classNames from "classnames";
+import PropTypes from "prop-types";
 
 class Dropdown extends React.Component {
+    constructor(props) {
+        super(props);
+        this.topNode = React.createRef();
 
-  displayName: 'Dropdown'
+        const value = [];
+        if (props.value && props.options) {
+            props.options.forEach((opt) => {
+                props.value.forEach((opt2) => {
+                    if (opt2.value === opt.value) {
+                        value.push(opt);
+                    }
+                });
+            });
+        }
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      selected: props.value || [],
-      isOpen: false
+        this.state = {
+            selected: value,
+            isOpen: false,
+        };
+
+        this.handleDocumentClick = this.handleDocumentClick.bind(this);
     }
-    this.mounted = true;
-    this.handleDocumentClick = this.handleDocumentClick.bind(this);
-  }
 
-  componentWillReceiveProps(newProps) {
-    if (newProps.value && newProps.value !== this.state.selected) {
-      this.setState({selected: newProps.value});
+    componentDidMount() {
+        document.addEventListener("click", this.handleDocumentClick, false);
     }
-  }
 
-  componentDidMount() {
-    document.addEventListener('click', this.handleDocumentClick, false);
-  }
-
-  componentWillUpdate(_newProps, newState) {
-    if (newState.selected !== this.state.selected && this.props.onChange) {
-      this.props.onChange(newState.selected);
+    componentWillUpdate(_newProps, newState) {
+        if (newState.selected !== this.state.selected && this.props.onChange) {
+            this.props.onChange(newState.selected);
+        }
     }
-  }
 
-  componentWillUnmount() {
-    this.mounted = false;
-    document.removeEventListener('click', this.handleDocumentClick, false);
-  }
+    componentWillUnmount() {
+        document.removeEventListener("click", this.handleDocumentClick, false);
+    }
 
-  handleMouseDown(event) {
+    handleMouseDown(event) {
+        if (event.type === "mousedown" && event.button !== 0) return;
+        event.stopPropagation();
+        event.preventDefault();
 
-    if (event.type == 'mousedown' && event.button !== 0) return;
-    event.stopPropagation();
-    event.preventDefault();
+        this.setState((pState) => ({
+            isOpen: !pState.isOpen,
+        }));
+    }
 
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
+    setValue(option) {
+        const { selected } = this.state;
+        const optionIndex = selected.indexOf(option);
+        const newState = {
+            selected: optionIndex === -1
+                ? selected.concat(option)
+                : selected.slice(0, optionIndex).concat(selected.slice(optionIndex + 1)),
+        };
+        this.setState(newState);
+    }
 
-  setValue(option) {
-    let { selected } = this.state;
-    let optionIndex = selected.indexOf(option);
-    let newState = {
-      selected: optionIndex === -1
-        ? selected.concat(option)
-        : selected.slice(0, optionIndex).concat(selected.slice(optionIndex + 1))
-    };
-    this.setState(newState);
-  }
-
-  renderOption (option) {
-    let optionClass = classNames({
-      'Dropdown-option': true,
-      'is-selected': this.state.selected.indexOf(option) !== -1
-    });
-
-    return <div key={option.value} className={optionClass} onClick={this.setValue.bind(this, option)}>{option.label}</div>
-  }
-
-  buildMenu() {
-    let ops = this.props.options.map((option) => {
-      if (option.type == 'group') {
-        let groupTitle = (<div className='title'>{option.name}</div>);
-        let _options = option.items.map((item) => this.renderOption(item));
+    renderOption(option) {
+        const optionClass = classNames({
+            "Dropdown-option": true,
+            "is-selected": this.state.selected.indexOf(option) !== -1,
+        });
 
         return (
-          <div className='group' key={option.name}>
-            {groupTitle}
-            {_options}
-          </div>
+            <div
+                role="option"
+                key={option.value}
+                className={optionClass}
+                onClick={this.setValue.bind(this, option)}
+                onKeyDown={this.setValue.bind(this, option)}
+                tabIndex="0"
+            >
+                {option.label}
+            </div>
         );
-      } else {
-        return this.renderOption(option);
-      }
-    })
-
-    return ops.length ? ops : <div className='Dropdown-noresults'>No options found</div>;
-  }
-
-  handleDocumentClick(event) {
-    if(this.mounted) {
-      if (!ReactDOM.findDOMNode(this).contains(event.target)) {
-        this.setState({isOpen:false});
-      }
     }
-  }
 
-  render() {
-    const { className, controlClassName, menuClassName, placeholder, noPreview } = this.props;
-    const { selected, isOpen } = this.state;
-    let value = selected.map(option => option.label).join(', ');
-    let menu = isOpen ? <div className={menuClassName}>{this.buildMenu()}</div> : null;
+    buildMenu() {
+        const ops = this.props.options.map((option) => {
+            if (option.type === "group") {
+                const groupTitle = (<div className="title">{option.name}</div>);
+                const options = option.items.map((item) => this.renderOption(item));
 
-    let dropdownClass = classNames({
-      'Dropdown': true,
-      'is-open': isOpen
-    }, className);
+                return (
+                    <div className="group" key={option.name}>
+                        {groupTitle}
+                        {options}
+                    </div>
+                );
+            }
+            return this.renderOption(option);
+        });
 
-    return (
-      <div className={dropdownClass}>
-        <div className={controlClassName} onMouseDown={this.handleMouseDown.bind(this)} onTouchEnd={this.handleMouseDown.bind(this)}>
-          {(!noPreview && value) || placeholder || "Select..."}
-          <span className='Dropdown-arrow' />
-        </div>
-        {menu}
-      </div>
-    );
-  }
+        return ops.length ? ops : <div className="Dropdown-noresults">No options found</div>;
+    }
 
+    renderHtmlOptions() {
+        const selectedValues = this.state.selected
+            .map((item) => String(item.value));
+
+        return this.props.options.map((option) => (
+            <option
+                key={option.value}
+                value={option.value}
+                selected={selectedValues.indexOf(String(option.value)) > -1}
+            >
+                {option.label}
+            </option>
+        ));
+    }
+
+    handleDocumentClick(event) {
+        if (!this.topNode.current.contains(event.target)) {
+            this.setState({ isOpen: false });
+        }
+    }
+
+    render() {
+        const {
+            className, controlClassName, menuClassName, placeholder, noPreview,
+        } = this.props;
+        const { selected, isOpen } = this.state;
+        const value = selected.map((option) => option.label).join(", ");
+        const menu = isOpen ? <div className={menuClassName}>{this.buildMenu()}</div> : null;
+
+        const dropdownClass = classNames({
+            Dropdown: true,
+            "is-open": isOpen,
+        }, className);
+
+        return (
+            <div ref={this.topNode} className={dropdownClass}>
+                <select
+                    multiple
+                    name={this.props.name}
+                    className="invisible-element"
+                >
+                    {this.renderHtmlOptions()}
+                </select>
+                <div role="listbox" tabIndex="0" className={controlClassName} onMouseDown={this.handleMouseDown.bind(this)} onTouchEnd={this.handleMouseDown.bind(this)}>
+                    {(!noPreview && value) || (noPreview && selected.length && `${selected.length} Selected`) || placeholder}
+                    <span className="Dropdown-arrow" />
+                </div>
+                {menu}
+            </div>
+        );
+    }
 }
-Dropdown.defaultProps = { controlClassName: 'Dropdown-control', menuClassName: 'Dropdown-menu'};
+
+Dropdown.propTypes = {
+    name: PropTypes.string,
+    value: PropTypes.array,
+    options: PropTypes.array,
+    placeholder: PropTypes.string,
+    onChange: PropTypes.func,
+    controlClassName: PropTypes.string,
+    menuClassName: PropTypes.string,
+    className: PropTypes.string,
+    noPreview: PropTypes.bool,
+};
+
+Dropdown.defaultProps = {
+    controlClassName: "Dropdown-control",
+    menuClassName: "Dropdown-menu",
+    onChange: () => {},
+    placeholder: "Select",
+};
+
 export default Dropdown;
